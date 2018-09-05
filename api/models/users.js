ed60c0c93a80de2e90faa3dd9263748391c7aad4
LucasLyoji <42807170+LucasLyoji@users.noreply.github.com>
2018-09-05 15:33:37 -0300

    Add files via upload

--- api/models/users.js
+++ api/models/users.js
@@ -1,11 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true},
+    email: { 
+        type: String, 
+        required: true,
+        unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
     first_name: { type: String, required: true},
     last_name: { type: String, required: true},
-    personal_fone: { type: String, required: true}
+    personal_fone: { type: String, required: true},
+    password: { type: String, required: true }
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
